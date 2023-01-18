cb24d202f9015039d9f76265ca37af3e698c085a
Ivan <Ivandgea@gmail.com>
2023-01-18 19:11:30 +0100

    Bonus 1 hecho

--- index.js
+++ index.js
@@ -62,6 +62,24 @@ for (i = 0; i < hacker1.length; i++) {
     }
 }
 
+//BONUS 1
+
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed hendrerit molestie ipsum, et cursus libero.Vivamus dui quam, tincidunt in mauris at, lobortis eleifend ex.Proin varius elementum eros, non scelerisque nisi dapibus vel.Praesent pretium orci lacus, fringilla aliquet odio sagittis pulvinar.Praesent luctus ipsum augue.Nam semper neque a tortor ultrices, non faucibus diam commodo.Nullam eleifend nulla non tellus egestas, id placerat ante luctus.Quisque nec augue ac diam tempor rutrum ac vitae sem.Praesent in condimentum ex, et laoreet nibh.Etiam eu dolor dignissim, pulvinar augue ac, vestibulum odio.Phasellus placerat convallis nulla, sed pellentesque odio posuere pretium.Duis auctor dui faucibus massa gravida varius.Vestibulum blandit condimentum elit eget euismod.Proin ac auctor orci, eleifend dictum enim.Nulla facilisi.Fusce eget erat dui. In hac habitasse platea dictumst.Mauris mi metus, hendrerit id leo vitae, ornare ultrices arcu.Suspendisse potenti.Aenean nec erat vitae magna condimentum maximus.Mauris vestibulum id nibh nec semper.Morbi ornare vitae lectus at gravida.Pellentesque ullamcorper purus ac orci blandit rutrum.Vivamus metus nunc, rhoncus ac ex a, facilisis efficitur diam.Suspendisse pellentesque aliquet consectetur.Praesent lorem magna, lobortis in arcu in, commodo blandit lectus.Suspendisse porttitor ultrices erat egestas rhoncus.Morbi a nisl neque.Nunc convallis leo ut bibendum volutpat.Suspendisse volutpat quam sed arcu varius, dapibus rutrum elit maximus.Vestibulum hendrerit vitae est id tristique.Maecenas luctus lacus euismod est ultrices facilisis."
+
+
+var words = longText.split(" ");
+console.log("Number of words in the string: " + words.length);
+
+var etCounter = 0;
+for (var i = 0; i < longText.length; i++) {
+    if (longText.substring(i, i + 2) === "et") {
+        etCounter++;
+    }
+}
+console.log(" 'et' aparece: " + etCounter + " en el Lorem Ipsum");
+
+
+//BONUS 2
 
 
 
